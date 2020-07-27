d257ab89087cbf32c611ea1749e343445f3d68bb
박종완 <whddhs6645@gmail.com>
2020-07-28 00:07:16 +0900

    Update test.go

--- test.go
+++ test.go
@@ -1,70 +1,74 @@
 package main
 
-import {
+import (
 	"flag"
 	"fmt"
 	"os"
-	"os/siginal"
+	"os/signal"
 	"syscall"
+
 	"github.com/bwmarrin/discordgo"
-}
+)
 
-//Variables used for command line parameters
-var{
+// Variables used for command line parameters
+var (
 	Token string
-}
+)
 
-func init(){
+func init() {
+
 	flag.StringVar(&Token, "t", "", "Bot Token")
 	flag.Parse()
 }
 
-func main(){
-	//Create a new Discord session using the provied bot token.
-	dg, err := discordgo.New("Bot "+ Token)
-	if err != nil{
-		fmt.Println("error creation Discord session,", err)
+func main() {
+
+	// Create a new Discord session using the provided bot token.
+	dg, err := discordgo.New("Bot " + Token)
+	if err != nil {
+		fmt.Println("error creating Discord session,", err)
 		return
 	}
-	//Register the messageCreate func as a callback for MessageCreate events.
+
+	// Register the messageCreate func as a callback for MessageCreate events.
 	dg.AddHandler(messageCreate)
 
-	//In this example, we only care about receving message events.
-	dg.Identify.Intents = discordgo.MakeIntent(discordgo.InstentsGuildMessages)
+	// In this example, we only care about receiving message events.
+	dg.Identify.Intents = discordgo.MakeIntent(discordgo.IntentsGuildMessages)
 
-	//Open a websocket connection to Discord and begin listening.
+	// Open a websocket connection to Discord and begin listening.
 	err = dg.Open()
-	if err != nil{
+	if err != nil {
 		fmt.Println("error opening connection,", err)
 		return
 	}
 
-	//Wait here until CTRL-C or other term signal is received.
-	fmt.Println("Bot is now running. Press CTRL-C to exit.")
+	// Wait here until CTRL-C or other term signal is received.
+	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
 	<-sc
 
-	//Cleanly close down the Discord session.
+	// Cleanly close down the Discord session.
 	dg.Close()
 }
 
-//This funcion will be called (due to AddHandler above) every time a new
-//message is created on any channel that the authenticated bot has access to.
-func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate){
-	//Ignore all messages created by the bot itself
-	//This isn't required in this specific example but it's a good practice.
-	if m.Author.ID == s.State.User.ID{
+// This function will be called (due to AddHandler above) every time a new
+// message is created on any channel that the authenticated bot has access to.
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+
+	// Ignore all messages created by the bot itself
+	// This isn't required in this specific example but it's a good practice.
+	if m.Author.ID == s.State.User.ID {
 		return
 	}
-
-	//If the message is "ping" reply with "Pong!"
+	// If the message is "ping" reply with "Pong!"
 	if m.Content == "ping" {
-		s.ChannelMessageSend(m.ChannelID, "pong!")
+		s.ChannelMessageSend(m.ChannelID, "Pong!")
 	}
 
-	//If the message is "pong" reply with "Ping!"
+	// If the message is "pong" reply with "Ping!"
 	if m.Content == "pong" {
-		s.ChannelMessageSend(m.ChannleID, "ping!")
+		s.ChannelMessageSend(m.ChannelID, "Ping!")
 	}
 }
